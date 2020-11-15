bd6a7d341f8dbbd7e850abed04bb8d797f99f177
Kugathasan Janarthanan <34956146+kujalk@users.noreply.github.com>
2020-11-15 19:50:43 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                  = "ap-southeast-1"
-  shared_credentials_file = "C:\\Users\\jana1\\.aws\\credentials"
+  shared_credentials_file = "C:\\Users\\myuser\\.aws\\credentials"
 }
